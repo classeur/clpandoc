78391204e93efe2489361ee045f7f7f0af4189b8
benweet <benoit.schweblin@gmail.com>
2015-12-14 21:13:58 +0000

    Switched to xelatex

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,25 @@
 FROM haskell:7.10
 
-# Start pandoc install
-ENV PANDOC_VERSION "1.15.1.1"
-RUN cabal update && cabal install pandoc-${PANDOC_VERSION}
-
 RUN apt-get update -y \
   && apt-get install -y --no-install-recommends \
   	curl \
-  	lmodern \
-    latex-xcolor \
-    texlive-latex-base \
-    texlive-xetex \
-    texlive-math-extra \
-    texlive-latex-extra \
-    texlive-fonts-extra \
-    texlive-bibtex-extra \
-    texlive-generic-extra \
-    texlive-generic-recommended \
-    fontconfig
+    wget \
+    unzip \
+    perl
 
-# Start node install
+# Install texlive
+RUN curl -SLO "http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz" \
+  && tar -xzf "install-tl-unx.tar.gz" \
+  && cd install-tl-* \
+  && (echo I | ./install-tl) \
+  && cd .. \
+  && rm -rf "install-tl*"
+
+# Install pandoc
+ENV PANDOC_VERSION "1.15.2.1"
+RUN cabal update && cabal install pandoc-${PANDOC_VERSION}
+
+# Install node
 RUN set -ex \
   && for key in \
     9554F04D7259F04124DE6B476D5A82AC7E37093B \
@@ -32,7 +32,7 @@ RUN set -ex \
     gpg --keyserver ha.pool.sks-keyservers.net --recv-keys "$key"; \
   done
 
-ENV NPM_CONFIG_LOGLEVEL info
+ENV NPM_CONFIG_LOGLEVEL warn
 ENV NODE_VERSION 4.2.1
 
 RUN curl -SLO "https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-linux-x64.tar.gz" \
@@ -41,6 +41,31 @@ RUN curl -SLO "https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-linux-
   && grep " node-v$NODE_VERSION-linux-x64.tar.gz\$" SHASUMS256.txt.asc | sha256sum -c - \
   && tar -xzf "node-v$NODE_VERSION-linux-x64.tar.gz" -C /usr/local --strip-components=1 \
   && rm "node-v$NODE_VERSION-linux-x64.tar.gz" SHASUMS256.txt.asc
+
+# install fonts
+RUN curl -SLO https://github.com/google/roboto/archive/master.tar.gz \
+  && tar -xzf master.tar.gz \
+  && mkdir -p ~/.fonts \
+  && cp roboto-master/out/RobotoTTF/*.ttf ~/.fonts/ \
+  && rm -rf master.tar.gz roboto-master
+
+RUN mkdir -p ~/.fonts \
+  && cd ~/.fonts \
+  && curl -SLO https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCJKsc-hinted.zip \
+  && unzip *.zip \
+  && rm *.zip \
+  && cd -
+
+RUN curl -SLO https://github.com/nathco/Office-Code-Pro/archive/master.tar.gz \
+  && tar -xzf master.tar.gz \
+  && mkdir -p ~/.fonts \
+  && cp Office-Code-Pro-master/Fonts/Office\ Code\ Pro/TTF/*.ttf ~/.fonts/ \
+  && rm -rf master.tar.gz Office-Code-Pro-master
+
+RUN apt-get install -y --no-install-recommends \
+  fontconfig
+
+ENV PATH /usr/local/texlive/2015/bin/x86_64-linux:$PATH
 
 RUN mkdir -p /root/.pandoc/templates
 COPY templates /root/.pandoc/templates
--- server.js
+++ server.js
@@ -1,197 +1,139 @@
-var spawn = require('child_process').spawn;
-var fs = require('fs');
-var cluster = require('cluster');
-var express = require('express');
-var app = express();
-var tmp = require('tmp');
+/* eslint no-console:0 */
+var spawn = require('child_process').spawn
+var fs = require('fs')
+var cluster = require('cluster')
+var express = require('express')
+var bodyParser = require('body-parser')
+var tmp = require('tmp')
 
-var timeout = process.env.PANDOC_TIMEOUT || 30000;
+var app = express()
+app.use(bodyParser.json())
+
+var timeout = process.env.PANDOC_TIMEOUT || 60000
 
 var outputFormats = [
-    'asciidoc',
-    'epub',
-    'epub3',
-    'latex',
-    'odt',
-    'pdf',
-    'rst',
-    'rtf',
-    'textile',
-    'docx',
-];
+	'asciidoc',
+	'context',
+	'epub',
+	'epub3',
+	'latex',
+	'odt',
+	'pdf',
+	'rst',
+	'rtf',
+	'textile',
+	'docx',
+]
 
 var highlightStyles = [
-    'pygments',
-    'kate',
-    'monochrome',
-    'espresso',
-    'zenburn',
-    'haddock',
-    'tango'
-];
+	'pygments',
+	'kate',
+	'monochrome',
+	'espresso',
+	'zenburn',
+	'haddock',
+	'tango'
+]
 
-var disabledExtensions = [
-    'blank_before_header',
-    'header_attributes',
-    'implicit_header_references',
-    'blank_before_blockquote',
-    'line_blocks',
-    'fancy_lists',
-    'example_lists',
-    'table_captions',
-    'simple_tables',
-    'multiline_tables',
-    'grid_tables',
-    'pandoc_title_block',
-    'inline_code_attributes',
-    'markdown_in_html_blocks',
-    'shortcut_reference_links',
-    'citations',
-];
+app.use(function(req, res, next) {
+	var options = req.body.options || {},
+		metadata = req.body.metadata || {},
+		params = [],
+		inputFormat = 'json',
+		outputFormat = req.body.format
+	outputFormat = outputFormats.indexOf(outputFormat) !== -1 ? outputFormat : 'pdf'
 
-var enabledExtensions = [
-    'lists_without_preceding_blankline',
-];
+	params.push('--latex-engine=xelatex')
+	params.push('--webtex=http://chart.apis.google.com/chart?cht=tx\&chf=bg,s,FFFFFF00\&chco=000000\&chl=')
+	options.toc && params.push('--toc')
+	options.tocDepth = parseInt(options.tocDepth, 10)
+	!isNaN(options.tocDepth) && params.push('--toc-depth', options.tocDepth)
+	options.highlightStyle = highlightStyles.indexOf(options.highlightStyle) !== -1 ? options.highlightStyle : 'kate'
+	params.push('--highlight-style', options.highlightStyle)
+	Object.keys(metadata).forEach(function(key) {
+		params.push('-M', key + '=' + metadata[key].toString())
+	})
 
-var switchableExtensions = [
-    'fenced_code_blocks',
-    'backtick_code_blocks',
-    'fenced_code_attributes',
-    'definition_lists',
-    'pipe_tables',
-    'strikeout',
-    'superscript',
-    'subscript',
-    'tex_math_dollars',
-    'tex_math_double_backslash',
-    'footnotes',
-    'inline_notes',
-    'hard_line_breaks',
-    'autolink_bare_uris',
-    'abbreviations',
-];
+	tmp.file({
+		postfix: '.' + outputFormat
+	}, function(err, path, fd, cleanupCallback) {
+		if (err) {
+			return next(err)
+		}
 
-var baseInputFormat = 'markdown';
-baseInputFormat = disabledExtensions.reduce(function(baseInputFormat, disabledExtension) {
-    return baseInputFormat + '-' + disabledExtension;
-}, baseInputFormat);
-baseInputFormat = enabledExtensions.reduce(function(baseInputFormat, enabledExtension) {
-    return baseInputFormat + '+' + enabledExtension;
-}, baseInputFormat);
+		var pandocError = ''
 
-app.post('/', function(req, res, next) {
+		function onError(err) {
+			cleanupCallback()
+			next(err)
+		}
 
-    var extensions = {},
-        options = {},
-        metadata = {},
-        params = [],
-        outputFormat = req.query.format;
-    outputFormat = outputFormats.indexOf(outputFormat) !== -1 ? outputFormat : 'pdf';
-    try {
-        extensions = JSON.parse(req.query.extensions);
-    } catch (e) {}
-    try {
-        options = JSON.parse(req.query.options);
-    } catch (e) {}
-    try {
-        metadata = JSON.parse(req.query.metadata);
-    } catch (e) {}
+		function onPandocError() {
+			res.statusCode = 400
+			cleanupCallback()
+			pandocError = pandocError || 'Unknown error'
+			console.error(pandocError)
+			res.end(pandocError)
+		}
 
-    var inputFormat = switchableExtensions.reduce(function(inputFormat, switchableExtension) {
-        return inputFormat + (extensions[switchableExtension] ? '+' : '-') + switchableExtension;
-    }, baseInputFormat);
+		function onTimeout() {
+			res.statusCode = 408
+			cleanupCallback()
+			res.end('Request timeout')
+		}
 
-    params.push('--webtex=http://chart.apis.google.com/chart?cht=tx\&chf=bg,s,FFFFFF00\&chco=000000\&chl=');
-    extensions.typographer && params.push('--smart');
-    options.toc && params.push('--toc');
-    options.tocDepth = parseInt(options.tocDepth);
-    !isNaN(options.tocDepth) && params.push('--toc-depth', options.tocDepth);
-    options.highlightStyle = highlightStyles.indexOf(options.highlightStyle) !== -1 ? options.highlightStyle : 'kate';
-    params.push('--highlight-style', options.highlightStyle);
-    Object.keys(metadata).forEach(function(key) {
-        params.push('-M', key + '=' + metadata[key].toString());
-    });
-
-    tmp.file({
-        postfix: outputFormat === 'pdf' ? '.pdf' : ''
-    }, function(err, path, fd, cleanupCallback) {
-        if (err) {
-            return next(err);
-        }
-
-        var pandocError = '';
-
-        function onError(err) {
-            cleanupCallback();
-            next(err);
-        }
-
-        function onPandocError() {
-            res.statusCode = 400;
-            cleanupCallback();
-            pandocError = pandocError || 'Unknown error';
-            console.error(pandocError);
-            res.end(pandocError);
-        }
-
-        function onTimeout() {
-            res.statusCode = 408;
-            cleanupCallback();
-            res.end('Request timeout');
-        }
-
-        var binPath = process.env.PANDOC_PATH || '/root/.cabal/bin/pandoc';
-        outputFormat = outputFormat === 'pdf' ? 'latex' : outputFormat;
-        params.push('-f', inputFormat, '-t', outputFormat, '-o', path);
-        var pandoc = spawn(binPath, params, {
-            stdio: [
-                'pipe',
-                'ignore',
-                'pipe'
-            ]
-        });
-        var timeoutId = setTimeout(function() {
-            timeoutId = undefined;
-            pandoc.kill();
-        }, timeout);
-        pandoc.on('error', onError);
-        pandoc.stdin.on('error', onError);
-        pandoc.stderr.on('data', function(data) {
-            pandocError += data.toString();
-        });
-        pandoc.on('close', function(code) {
-            if (!timeoutId) {
-                return onTimeout();
-            }
-            clearTimeout(timeoutId);
-            if (code) {
-                return onPandocError();
-            }
-            var readStream = fs.createReadStream(path);
-            readStream.on('open', function() {
-                readStream.pipe(res);
-            });
-            readStream.on('close', function() {
-                cleanupCallback();
-            });
-            readStream.on('error', onPandocError);
-        });
-        req.pipe(pandoc.stdin);
-    });
-});
+		var binPath = process.env.PANDOC_PATH || '/root/.cabal/bin/pandoc'
+		outputFormat = outputFormat === 'pdf' ? 'latex' : outputFormat
+		params.push('-f', inputFormat, '-t', outputFormat, '-o', path)
+		var pandoc = spawn(binPath, params, {
+			stdio: [
+				'pipe',
+				'ignore',
+				'pipe'
+			]
+		})
+		var timeoutId = setTimeout(function() {
+			timeoutId = undefined
+			pandoc.kill()
+		}, timeout)
+		pandoc.on('error', onError)
+		pandoc.stdin.on('error', onError)
+		pandoc.stderr.on('data', function(data) {
+			pandocError += data.toString()
+		})
+		pandoc.on('close', function(code) {
+			if (!timeoutId) {
+				return onTimeout()
+			}
+			clearTimeout(timeoutId)
+			if (code) {
+				return onPandocError()
+			}
+			var readStream = fs.createReadStream(path)
+			readStream.on('open', function() {
+				readStream.pipe(res)
+			})
+			readStream.on('close', function() {
+				cleanupCallback()
+			})
+			readStream.on('error', onPandocError)
+		})
+		pandoc.stdin.end(req.body.ast)
+	})
+})
 
 if (process.env.USE_CLUSTER && cluster.isMaster) {
-    var count = require('os').cpus().length;
-    for (var i = 0; i < count; i++) {
-        cluster.fork();
-    }
-    cluster.on('exit', function() {
-        console.log('Worker died. Spawning a new process...');
-        cluster.fork();
-    });
+	var count = require('os').cpus().length
+	for (var i = 0; i < count; i++) {
+		cluster.fork()
+	}
+	cluster.on('exit', function() {
+		console.log('Worker died. Spawning a new process...')
+		cluster.fork()
+	})
 } else {
-    var port = 3000;
-    app.listen(port, function() {
-        console.log('HTTP server started: http://localhost:' + port);
-    });
+	var port = 3000
+	app.listen(port, function() {
+		console.log('HTTP server started: http://localhost:' + port)
+	})
 }
